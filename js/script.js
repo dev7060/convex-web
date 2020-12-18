f0270589608e42cce7141449196aaddd911dde30
deV <46674147+dev7060@users.noreply.github.com>
2020-12-18 08:40:20 +0530

    add js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById('loader').style.display = "none";
+    document.getElementById('mainUI').removeAttribute("style");
+    document.getElementById('copyright').removeAttribute("style");
+};
